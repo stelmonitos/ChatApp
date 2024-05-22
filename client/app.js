c32de3de718114ed6d2e2ab8c7004e649441c2b8
Jakub Stelmasiak <stelmon3@gmail.com>
2024-05-22 16:09:56 +0200

    working chat

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 // referencje do elementów HTML
 const loginForm = document.querySelector('#welcome-form');
 const messagesSection = document.querySelector('#messages-section');
@@ -6,51 +8,56 @@ const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 
+// nasłuchiwacz zdarzeń
+
 // globalne zmienne
 var userName = '';
 
 // funkcja logowania
 const login = (e) => {
     e.preventDefault(); // blokowanie domyślnego zachowania przeglądarki
-    // Tutaj możesz dodać logikę logowania, np. przypisanie wartości z userNameInput do zmiennej userName
     userName = userNameInput.value.trim();
     if (userName) {
         // Przełączanie widoczności sekcji
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
-    } else if( userName == '' ){
+        socket.emit('join', userName )
+    } else if (userName == '') {
         alert('Please enter your name');
     }
-    console.log('login')
+    console.log('loged in as '+ userName);
 }
 
-function addMessage (author, content) {
+function addMessage(author, content) {
     const message = document.createElement('li');
     message.classList.add('message')
     message.classList.add('message--received')
-    if(author === userName){
+    if (author === userName) {
         message.classList.add('message--self')
     }
-    message.innerHTML=`
-        <h3 class="message__author">${userName === author ? 'You' : author}</h3>
-        <div class="message__content">
-            ${content}
-        </div>
+    message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You (' + userName + ')' : author}</h3>
+    <div class="message__content">
+    ${content}
+    </div>
     `;
     messagesList.appendChild(message);
 }
 
 function sendMessage(e) {
     e.preventDefault();
-    let message = messageContentInput.value.trim();
-    if(message == ''){
-        alert('Please enter a message');
-    } else {
-        addMessage(userName, messageContentInput.value);
+    
+    let messageContent = messageContentInput.value;
+    
+    if (!messageContent.length) {
+        alert('You have to type something!');
+    }
+    else {
+        addMessage(userName, messageContent);
+        socket.emit('message', { author: userName, content: messageContent })
         messageContentInput.value = '';
     }
 }
-
-// nasłuchiwacz zdarzeń
 loginForm.addEventListener('submit', login);
-addMessageForm.addEventListener('submit', sendMessage)
+socket.on('message', ({ author, content }) => addMessage(author, content));
+addMessageForm.addEventListener('submit', sendMessage);
--- server.js
+++ server.js
@@ -1,19 +1,46 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-
+const socket = require('socket.io');
 app.use(express.static(path.join(__dirname, './client')));
+
+
+const server = app.listen(8000, () => {
+    console.log('Server runs on port 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+    });
+    socket.on('join', (login) => {
+        users.push({ name: login, id: socket.id })
+        console.log('Oh, I\'ve got something from...');
+    });
+    socket.on('disconnect', () => {
+        console.log('Oh, socket ' + socket.id + ' has left')
+        users.splice(users.indexOf(socket.id), 1);
+    });
+    console.log('I\'ve added a listener on message and disconnect events \n');
+});
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
-    
 })
 
-app.listen(8000, () => {
-    console.log('Server runs on port 8000');
-});
 
 const messages = [
 
 ];
 
+const users = [
+    
+];
+console.log('users:', users);
